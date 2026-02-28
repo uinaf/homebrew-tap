e12320468d003ea30a590a6eb8d04f554db63198
glitch418x <glitch@uinaf.dev>
2026-02-28 14:31:17 +0300

    healthd 0.5.0

--- Formula/healthd.rb
+++ Formula/healthd.rb
@@ -2,15 +2,15 @@ class Healthd < Formula
   desc "Pluggable local host health-check daemon"
   homepage "https://github.com/uinaf/healthd"
   license "MIT"
-  version "0.4.0"
+  version "0.5.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/uinaf/healthd/releases/download/v0.4.0/healthd_v0.4.0_darwin_arm64.tar.gz"
-      sha256 "06509906bea37659f2565050b1f948dae46bbaa2fc324971987652b83525546d"
+      url "https://github.com/uinaf/healthd/releases/download/v0.5.0/healthd_v0.5.0_darwin_arm64.tar.gz"
+      sha256 "6ef77c6da1c73102d35611d3a5894a406cd304e3e68fa2da5a89b6c4402377fe"
     else
-      url "https://github.com/uinaf/healthd/releases/download/v0.4.0/healthd_v0.4.0_darwin_amd64.tar.gz"
-      sha256 "ba0f0c3a8eb72f1de53a55d6bbe509ac1efb0beb9d3d31e0f9dc7b7a8e9a1d95"
+      url "https://github.com/uinaf/healthd/releases/download/v0.5.0/healthd_v0.5.0_darwin_amd64.tar.gz"
+      sha256 "f82856202b6e78abc306302e8b9477946f6f6c80ba4b194c0b9afc3a0b10d6b0"
     end
   end
 
