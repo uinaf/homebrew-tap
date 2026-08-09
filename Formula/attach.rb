21756ec12c18acbbfc50ed43af18add5ecf8243a
Altay <altay@hey.com>
2026-08-09 03:46:00 +0300

    test(attach): capture legacy help output

--- Formula/attach.rb
+++ Formula/attach.rb
@@ -13,7 +13,7 @@ class Attach < Formula
   end
 
   test do
-    assert_match "attach put", shell_output("#{bin}/attach --help", 2)
-    assert_match "attach put", shell_output("#{bin}/gh-attach --help", 2)
+    assert_match "attach put", shell_output("#{bin}/attach --help 2>&1", 2)
+    assert_match "attach put", shell_output("#{bin}/gh-attach --help 2>&1", 2)
   end
 end
