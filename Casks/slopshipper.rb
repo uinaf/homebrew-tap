ced22a27b9c4776b6b7a08969186fca7b9a6b76c
Altay <altay@hey.com>
2026-09-08 23:21:06 +0300

    chore: retire slopmachine Homebrew distribution (#15)
    * chore: retire slopmachine Homebrew distribution
    
    * docs: clarify the legacy cask retirement notice

--- Casks/slopshipper.rb
+++ Casks/slopshipper.rb
@@ -4,10 +4,10 @@ cask "slopshipper" do
 
   url "https://github.com/uinaf/ffss"
   name "slopshipper"
-  desc "Retired; replaced by slopmachine in uinaf/ffss"
+  desc "Autonomous coding CLI"
   homepage "https://github.com/uinaf/ffss"
 
   binary "slopshipper"
 
-  disable! date: "2026-08-16", because: "slopshipper was renamed slopmachine and lives in uinaf/ffss; install with: brew install --cask uinaf/tap/slopmachine"
+  disable! date: "2026-08-16", because: "the CLI is retired; use the slopmachine execution skill at https://github.com/uinaf/ffss"
 end
